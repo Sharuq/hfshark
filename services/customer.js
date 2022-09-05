dffff2eb0f40f88aa70844df3ec4c0dd64a1d68f
Jishnu Ramesh <jish_r@live.com>
2022-09-05 18:02:41 +1000

    added async

--- services/customer.js
+++ services/customer.js
@@ -8,6 +8,19 @@ function getAllCustomers() {
             {'customer_id': '90', 'name': 'John'}]
 }
 
+async function getAllCustomersAsync() {
+
+        return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([{'customer_id': 100, 'name': 'Michael'},
+            {'customer_id': 88, 'name': 'Noah'},
+            {'customer_id': 4, 'name': 'James'},
+            {'customer_id': 90, 'name': 'Carla'},
+            {'customer_id': 8, 'name': 'Noah'}]);
+    }, 2000);
+  });
+}
 
 
-export default {getAllCustomers}
+
+export default {getAllCustomers, getAllCustomersAsync}
